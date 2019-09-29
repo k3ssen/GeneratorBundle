3ab5e90348be567e0626da5be14ee24900899b46
Kevin <kevin3ssen@gmail.com>
2019-09-29 10:38:49 +0200

    fix treebuilder deprecation

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -13,8 +13,8 @@ class Configuration implements ConfigurationInterface
      */
     public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('generator');
+        $treeBuilder = new TreeBuilder('generator');
+        $rootNode = $treeBuilder->getRootNode();
         $rootNode
             ->children()
                 ->arrayNode('attributes')
