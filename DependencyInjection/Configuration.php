158d7823402fc8b407dce0612506152bcd88f233
Kevin <kevin3ssen@gmail.com>
2019-10-04 20:55:34 +0200

    reserve deprecation-fix, because non-deprecated method is not supported by LTS version

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -13,8 +13,8 @@ class Configuration implements ConfigurationInterface
      */
     public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder('generator');
-        $rootNode = $treeBuilder->getRootNode();
+        $treeBuilder = new TreeBuilder();
+        $rootNode = $treeBuilder->root('generator');
         $rootNode
             ->children()
                 ->arrayNode('attributes')
